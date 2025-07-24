c7ae412f9d82991da6b3458fddbe001ca1450f97
Shadrack <shadrackkemei2019@gmail.com>
2025-07-24 18:17:16 +0300

    Refactor code structure for improved readability and maintainability

---
+++ constants/projects/page.jsx
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'CineVerse',
+    description: 'A movie review platform that allows users to browse movies and post reviews.',
+    techStack: ['React', 'Flask', 'SQLAlchemy', 'Formik', 'React Router'],
+    githubUrl: 'https://github.com/Shadrack-Kipkemei/CineVerse',
+    liveUrl: 'https://cine-verse-bice.vercel.app/',
+    image: '/Cinverse.png'
+  },
+  {
+    id: 2,
+    title: 'CareBridge Backend API',
+    description: 'A full-featured backend API to support a charitable donations platform.',
+    techStack: ['Flask', 'JWT', 'OAuth', 'PayPal SDK', 'PostgreSQL'],
+    githubUrl: 'https://github.com/Shadrack-Kipkemei/carebridge-backend',
+    liveUrl: 'https://carebridge-backend-fys5.onrender.com/',
+    image: '/Carebridge backend.png'
+  }
+]
